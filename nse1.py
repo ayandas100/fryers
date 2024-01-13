38e171f6d2bd86c4478a63646b284e8248b1d7f9
ayandas100 <ayandas100@gmail.com>
2024-01-13 09:41:31 +0530

    new files

--- nse1.py
+++ nse1.py
@@ -17,6 +17,7 @@ def nse_headers():
     return {'Accept': '*/*',
             'Accept-Language': 'en-US,en;q=0.5',
             'Host': 'www1.nseindia.com',
+
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
             'X-Requested-With': 'XMLHttpRequest'
             }
--- test/test1.py
+++ test/test1.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import mysql.connector
 
-conn = create_engine("mysql://ayan_1:A380b747@127.0.0.1/tradedb")
-df = pd.read_sql("select * from nifty_50 order by trade_date desc limit 5",conn)
+conn = create_engine("mysql://ayan:A380b747@127.0.0.1/tradedb")
+df = pd.read_sql("select * from nifty_history order by current_date_time desc limit 10",conn)
 print(df.head())
+
